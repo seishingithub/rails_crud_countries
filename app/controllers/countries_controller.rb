5938c4b3cdfa08256f81abe7b6d81db810605b77
Peggy Griffin <seishingithub@gmail.com>
2014-06-07 16:58:28 -0600

    User can edit and update country

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -14,6 +14,21 @@ class CountriesController < ApplicationController
     end
   end
 
+  def show
+    @country = Country.find(params[:id])
+  end
+
+  def edit
+    @country = Country.find(params[:id])
+  end
+
+  def update
+    @country = Country.find(params[:id])
+    @country.update_attributes!(country_params)
+
+    redirect_to countries_path
+  end
+
   private
   def country_params
     params.require(:country).permit(:country_name, :language_spoken)
--- spec/countries/countries_spec.rb
+++ spec/countries/countries_spec.rb
@@ -11,4 +11,26 @@ feature 'Manage Countries' do
     expect(page).to have_content 'Mexico'
     expect(page).to have_content 'Spanish'
   end
+
+  scenario 'User can edit and update countries' do
+    visit '/'
+    click_on 'Add country'
+    fill_in 'Country name', with: 'Mexico'
+    fill_in 'Language spoken', with: 'Spanish'
+    click_on 'Create country'
+    expect(page).to have_content 'Mexico'
+    expect(page).to have_content 'Spanish'
+    click_on 'Mexico'
+    expect(page).to have_content 'Mexico'
+    expect(page).to have_content 'Spanish'
+    click_on 'Edit country'
+    fill_in 'Country name', with: 'Brazil'
+    fill_in 'Language spoken', with: 'Portuguese'
+    click_on 'Update country'
+    expect(page).to have_content 'Brazil'
+    expect(page).to have_content 'Portuguese'
+    expect(page).to have_no_content 'Mexico'
+    expect(page).to have_no_content 'Spanish'
+
+  end
 end
